9f166b42e4420a220e2a4d0db6c992580d8bb877
michel <michel@localhost>
2015-05-16 11:23:46 -0400

    Import cleanup

--- src/main/java/me/tabinol/factoid/parameters/Parameters.java
+++ src/main/java/me/tabinol/factoid/parameters/Parameters.java
@@ -23,12 +23,10 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.logging.Level;
+
+import me.tabinol.factoidapi.parameters.IParameters;
 
 import org.bukkit.Material;
-
-import me.tabinol.factoid.Factoid;
-import me.tabinol.factoidapi.parameters.IParameters;
 
 
 /**
